cd0e2b226e52f8db409e790a93da0b9954cf1afd
mastersenpai05 <pampangavergil0405@gmail.com>
2022-05-10 19:32:56 +0800

    1819: Rebase to android 9

--- omni_1819.mk
+++ omni_1819.mk
@@ -6,20 +6,19 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from 1819 device
-$(call inherit-product, device/vivo/1819/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := 1819
 PRODUCT_NAME := omni_1819
 PRODUCT_BRAND := vivo
-PRODUCT_MODEL := vivo 1819
+PRODUCT_MODEL := vivo V15
 PRODUCT_MANUFACTURER := vivo
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
